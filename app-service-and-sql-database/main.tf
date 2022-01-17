4fa1a258276db78c3060ed68bb73413fca65800d
platforminc <platforminc@hotmail.com>
2022-01-17 00:39:47 +0000

    Adding block to TF file

--- app-service-and-sql-database/main.tf
+++ app-service-and-sql-database/main.tf
@@ -1,6 +1,10 @@
+provider "azurerm" {
+   features {}
+}
+
 resource "azurerm_resource_group" "RG-Terraform" {
-  name     = "terraform-resource-group"
-  location = "West Europe"
+  name     = "tf_build"
+  location = "UK South"
 }
 
 resource "azurerm_app_service_plan" "ASP-TerraForm" {
@@ -41,7 +45,7 @@ resource "azurerm_sql_server" "terraform-sqlserver" {
   resource_group_name          = azurerm_resource_group.RG-Terraform.name
   location                     = azurerm_resource_group.RG-Terraform.location
   version                      = "12.0"
-  administrator_login          = "houssem"
+  administrator_login          = "toyin"
   administrator_login_password = "4-v3ry-53cr37-p455w0rd"
 }
 
